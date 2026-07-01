25824ae1874feeaf05eaffe3fa43dad151505ef0
RishaanJ <rishaanjain@gmail.com>
2026-06-30 22:00:15 -0700

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Pixelify_Sans } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -41,6 +42,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} ${pixelify.variable} h-full`}
     >
+      <Analytics />
       <body className="min-h-full flex flex-col bg-paper text-ink">
         {children}
       </body>
